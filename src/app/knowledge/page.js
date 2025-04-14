9c7107a84fc88e7c1496b61257c28ec7c20b1508
Yuma Moriya <y.moriya.113@gmail.com>
2025-04-15 02:06:50 +0900

    エンドポイントの修正。#36

--- src/app/knowledge/page.js
+++ src/app/knowledge/page.js
@@ -61,7 +61,7 @@ export default function KnowledgePage() {
       }
       
       // APIからナレッジデータを取得（キーワード検索を追加）
-      const url = new URL(`${process.env.NEXT_PUBLIC_ENDPOINT}/knowledge/`);
+      const url = new URL(`${process.env.NEXT_PUBLIC_ENDPOINT}/knowledge/popular/`);
       url.searchParams.append('limit', '20');
       url.searchParams.append('offset', '0');
       
